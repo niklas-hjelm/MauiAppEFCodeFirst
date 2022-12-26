0d47b11731612bba1e085ece1d166c3095cf5d6a
Niklas <niklas.hjelm@iths.se>
2022-12-26 14:24:37 +0100

    Add project files.

---
+++ DataAccess/MyDemoContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MauiApp1.DataAccess;
+
+public class MyDemoContext : DbContext
+{
+    public DbSet<Person> People { get; set; }
+
+    public MyDemoContext()
+    {
+        
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=DemoDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+    }
+}
---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using MauiApp1.DataAccess;
+
+namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+        count++;
+        using (var context = new MyDemoContext())
+        {
+            context.People.Add(new Person() { Name = $"Person{count}" });
+            context.SaveChanges();
+        }
+
+        CounterBtn.Text = $"{count} people added";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
